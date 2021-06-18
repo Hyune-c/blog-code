51dc7a5528f25019560468e519d0c29fdcd11234
choibyunghyeon <choi8608@gmail.com>
2021-06-19 00:07:16 +0900

    [signup-security] 🔧 초기 설정

---
+++ signup-security/src/test/java/com/example/signupsecurity/SignupSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signupsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
