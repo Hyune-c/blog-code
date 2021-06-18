42c572e485dda16293b9f96e3686fe69d09c0dde
choibyunghyeon <choi8608@gmail.com>
2021-06-18 23:52:36 +0900

    Initial commit

---
+++ src/test/java/com/example/blogcode/BlogCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCodeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
