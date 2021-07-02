7585ef197040d4de223841dc659115a224c1bb39
choibyunghyeon <choi8608@gmail.com>
2021-07-02 17:48:29 +0900

    [exception-logging] ✨️ MDC 적용

--- exception-logging/src/main/java/com/hyune/exceptionlogging/web/TestController.java
+++ exception-logging/src/main/java/com/hyune/exceptionlogging/web/TestController.java
@@ -1,6 +1,7 @@
 package com.hyune.exceptionlogging.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class TestController {
     public void test() {
         System.out.println("system out");
         log.info("### log info");
-        throw new RuntimeException("test!!");
+        log.info("### MDC transactionId={}", MDC.get("transactionId"));
+        throw new RuntimeException("test!! " + MDC.get("transactionId"));
     }
 }
