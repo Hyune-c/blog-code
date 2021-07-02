7057863ce352969697ebda96c6a853ff5a7ff748
choibyunghyeon <choi8608@gmail.com>
2021-07-03 00:33:39 +0900

    [exception-logging] ♻️ async 테스트 정상 작동하게 개선

--- exception-logging/src/main/java/com/hyune/exceptionlogging/config/AsyncConfiguration.java
+++ exception-logging/src/main/java/com/hyune/exceptionlogging/config/AsyncConfiguration.java
@@ -1,11 +1,26 @@
 package com.hyune.exceptionlogging.config;
 
+import com.hyune.exceptionlogging.config.logging.MDCTaskDecorator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfiguration implements AsyncConfigurer {
 
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+
+        taskExecutor.setTaskDecorator(new MDCTaskDecorator());
+        taskExecutor.setCorePoolSize(20);
+        taskExecutor.setMaxPoolSize(50);
+        taskExecutor.setQueueCapacity(100);
+
+        return taskExecutor;
+    }
 }
