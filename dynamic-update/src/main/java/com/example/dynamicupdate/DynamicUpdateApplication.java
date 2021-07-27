1e545acf71a35b9ba586836b4290093cab0b5e92
choibyunghyeon <choi8608@gmail.com>
2021-07-27 16:44:02 +0900

    [dynamic-update] 🔧 init

---
+++ dynamic-update/src/main/java/com/example/dynamicupdate/DynamicUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamicupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicUpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicUpdateApplication.class, args);
+    }
+
+}
